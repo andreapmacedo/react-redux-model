e243d64c4b1500ec96888a11ce499a0f5cd7356f
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-03 20:45:32 -0300

    Feat: provider-01.

--- react-redux-app/src/redux/index.js
+++ react-redux-app/src/redux/index.js
@@ -1,6 +1,10 @@
-// ./src/redux/index.js
 import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore();
+const INITIAL_STATE = { count: 0 };
+
+const reducer = (state = INITIAL_STATE, action) => state;
+
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
