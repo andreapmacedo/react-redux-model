64c1c6874b473aca287983dbc082a1694138e712
andreapmacedo <aa.andremacedo@gmail.com>
2023-05-04 08:42:36 -0300

    Feat: Adiciona o actionCreator.

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { actionCreator } from './redux/actions';
 
 // function App(props) {
 class App extends React.Component {
   render() {
-    const { countState } = this.props;
+    // const { countState } = this.props;
+    const { dispatch, countState } = this.props;
     return (
       <div>
         <h1>Contador</h1>
         <h2>{ countState }</h2>
-        <button>Incrementa 1</button>
-        <button>Incrementa 5</button>
+        {/* <button>Incrementa 1</button>
+        <button>Incrementa 5</button> */}
+        <button onClick={() => dispatch(actionCreator())}>Incrementa 1</button>
+        <button onClick={() => dispatch(actionCreator(5))}>Incrementa 5</button>
       </div>
     );
   }
